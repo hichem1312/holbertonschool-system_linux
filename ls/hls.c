7026aab301a28aef728fead814a65c610d9f9a3f
hichem <hichem@hichem.com>
2023-03-25 04:02:55 +0100

    program that lists the content of the current directory

---
+++ ls/hls.c
@@ -0,0 +1,19 @@
+#include "ls.h"
+/**
+ * main - ls program
+ * @argc: the number of arguments on the command line
+ * @argv: argv
+ * Return: content of the current diractory
+*/
+int main(int argc, char *argv[])
+{
+    DIR *dir;
+    struct dirent *read;
+    if (argc < 2)
+        argv[0] = "./";
+    dir = opendir(argv[0]);
+    while ((read = readdir(dir)) != NULL)
+        printf("%s\n", read->d_name);
+    closedir(dir);
+    return(0);
+}
