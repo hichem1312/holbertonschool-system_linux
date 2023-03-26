7c0d544db13b1f35a3198fc8bd6b44e2569fd0a2
hichem <hichem@hichem.com>
2023-03-26 03:38:33 +0100

    errors handling

--- ls/hls.c
+++ ls/hls.c
@@ -9,15 +9,32 @@ int main(int argc, char *argv[])
 {
 	DIR *dir;
 	struct dirent *read;
+	int i;
 
 	if (argc < 2)
 		argv[0] = "./";
-	dir = opendir(argv[0]);
-	while ((read = readdir(dir)) != NULL)
+	for (i = 1; i < argc; i++)
 	{
-		if (read->d_name[0] != '.')
-			printf("%s  ", read->d_name);
+
+		dir = opendir(argv[i]);
+		if (dir)
+		{
+			printf("%s:\n", argv[i]);
+				while ((read = readdir(dir)) != NULL)
+			{
+				if (read->d_name[0] != '.')
+					printf("%s  ", read->d_name);
+			}
+			printf("\n");
+		}
+		else
+		{
+			fprintf(stderr, "./hls: cannot access '%s': ", argv[i]);
+			perror("");
+		}
+		
 	}
+	
 	closedir(dir);
 	return (0);
 }
