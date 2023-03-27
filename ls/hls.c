71afe8c84cea0dfcc5f6b978d13219383a9aa980
hichem <hichem@hichem.com>
2023-03-28 00:37:11 +0100

    some options

--- ls/hls.c
+++ ls/hls.c
@@ -10,7 +10,7 @@ int main(int argc, char *argv[])
 	DIR *dir;
 	struct dirent *read;
 	int i, j, a = 0, A = 0, l = 0, n = argc;
-	int x = argc - 1;
+	/*int x = argc - 1;*/
 	struct stat buf;
 
 	if (argc < 2)
@@ -36,13 +36,13 @@ int main(int argc, char *argv[])
 					printf("%s:\n", argv[i]);
 				while ((read = readdir(dir)) != NULL)
 				{
-					if ((read->d_name[0] != '.' || (a > 0)) && (l > 0))
+					if (((read->d_name[0] != '.' || (a > 0)) || ((A > 0) && (read->d_name[1] != 46 && strlen(read->d_name) > 1))) && (l > 0))
 						printf("%s\n", read->d_name);
-					else if ((read->d_name[0] != '.' || (a > 0)) && (l == 0))
+					else if (((read->d_name[0] != '.' || (a > 0)) || ((A > 0) && (read->d_name[1] != 46 && strlen(read->d_name) > 1))) && (l == 0))
 						printf("%s  ", read->d_name);
 				}
-				/*if (i != x)
-					printf("\n");*/
+				if (l == 0)
+					printf("\n");
 				closedir(dir);
 			}
 			else if (lstat(argv[i], &buf) == 0)
@@ -60,7 +60,7 @@ int main(int argc, char *argv[])
 				fprintf(stderr, "%s: cannot access %s: ", argv[0], argv[i]);
 				perror("");
 			}
-			if (i != x || (n == 1) || (l == 0 && a == 0 && A == 0))
+			if (i != n - 1 /*|| (n == 2) || (l == 0 && a == 0 && A == 0)*/)
 				printf("\n");
 		}
 	}
