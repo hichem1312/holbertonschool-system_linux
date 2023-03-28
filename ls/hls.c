de2d16a268fb15cff1fedce7a152b9238c3dab90
hichem <hichem@hichem.com>
2023-03-28 02:44:27 +0100

    some options

--- ls/hls.c
+++ ls/hls.c
@@ -9,12 +9,19 @@ int main(int argc, char *argv[])
 {
 	DIR *dir;
 	struct dirent *read;
-	int i, j, a = 0, A = 0, l = 0, n = argc;
+	int i, j, a = 0, A = 0, l = 0, n = argc, f = 0, total = 0, v = 0;
 	/*int x = argc - 1;*/
 	struct stat buf;
 
 	if (argc < 2)
 		argv[0] = "./";
+	for (int p = 1; p < argc; p++)
+	{
+		if (!(opendir(argv[p])))
+		{
+			v += 1;
+		}
+	}
 	for (j = 1; j < argc; j++)
 	{
 		if ((argv[j][0] == '-') && (argv[j][1] == '1'))
@@ -23,8 +30,26 @@ int main(int argc, char *argv[])
 			a += 1;
 		else if ((argv[j][0] == '-') && (argv[j][1] == 'A'))
 			A += 1;
+		if (!(opendir(argv[j])) && (v < 2) && (lstat(argv[j], &buf) == 0))
+		{
+			printf("%s\n", argv[j]);
+			f += 1;
+		}
+		else if (!(opendir(argv[j])) && (v > 1) && (lstat(argv[j], &buf) == 0))
+		{
+			printf("%s ", argv[j]);
+			f += 1;
+		}
+		else
+		{
+			total += 1;
+		}
 	}
-	n = n - (l + a + A);
+	if ((f > 0 && total > 0) && (v < 2))
+		printf("\n");
+	else if ((f > 0 && total > 0) && (v > 1))
+		printf("\n\n");
+	n = n - (l + a + A + f);
 	for (i = 1; i < argc; i++)
 	{
 		if (argv[i][0] != '-')
@@ -32,7 +57,7 @@ int main(int argc, char *argv[])
 			dir = opendir(argv[i]);
 			if (dir)
 			{
-				if (n > 2)
+				if (n > 2 || f > 0)
 					printf("%s:\n", argv[i]);
 				while ((read = readdir(dir)) != NULL)
 				{
@@ -52,15 +77,15 @@ int main(int argc, char *argv[])
 					fprintf(stderr, "%s: cannot open directory %s: ", argv[0], argv[i]);
 					perror("");
 				}
-				else
-					printf("%s\n", argv[i]);
+				/*else
+					printf("%s\n", argv[i]);*/
 			}
 			else
 			{
 				fprintf(stderr, "%s: cannot access %s: ", argv[0], argv[i]);
 				perror("");
 			}
-			if (i <= n - 1 && argc != 2/*|| (n == 2) || (l == 0 && a == 0 && A == 0)*/)
+			if (i < n - 1 && argc != 2/*|| (n == 2) || (l == 0 && a == 0 && A == 0)*/)
 				printf("\n");
 		}
 	}
