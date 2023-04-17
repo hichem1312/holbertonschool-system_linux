d1cd680ba0b2bc9780c744ee9eec533f38aeb48e
hichem <hichem@hichem.com>
2023-04-17 23:06:21 +0100

    getline function

--- getline/_getline.c
+++ getline/_getline.c
@@ -7,33 +7,33 @@
  **/
 char *_getline(const int fd)
 {
-    char buff[READ_SIZE];
-    size_t check;
-    char *new_line = NULL;
-    int i = 0;
+	char buff[READ_SIZE];
+	size_t check;
+	char *new_line = NULL;
+	int i = 0;
 
-    while ((check = read(fd, buff + i, 1)) > 0)
-    {
-        if (buff[i] == '\n')
-        {
-            break;
-        }
-        i++;
-    }
-    
-    if (i >= 0)
-    {
-        new_line = malloc((i + 1) * sizeof(char));
-        if (!new_line) 
-        {
-            return(NULL);
-        }
-        strncpy(new_line, buff, i);
-        new_line[i] = '\0';
-    }
-    if (check == 0 && i <= 0)
-    {
-       return(NULL);
-    }
-    return(new_line);
+	while ((check = read(fd, buff + i, 1)) > 0)
+	{
+		if (buff[i] == '\n')
+		{
+			break;
+		}
+		i++;
+	}
+
+	if (i >= 0)
+	{
+		new_line = malloc((i + 1) * sizeof(char));
+		if (!new_line) 
+		{
+			return(NULL);
+		}
+		strncpy(new_line, buff, i);
+		new_line[i] = '\0';
+	}
+	if (check == 0 && i <= 0)
+	{
+		return(NULL);
+	}
+	return(new_line);
 }
