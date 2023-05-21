781f43c49cd123867610569c91cd803f6cc6858d
hichem <hichem@hichem.com>
2023-05-21 20:29:37 +0100

    a program that displays the information contained in the ELF program headers

---
+++ readelf/2-hreadelf.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - displays the information contained in the ELF file header.
+ * @argc: number of argements
+ * @argv: argument vector
+ * @env:  environment
+ * Return: -1 or 0
+ */
+int main(int argc, char **argv, char **env)
+{
+	char *command[] = {"/usr/bin/readelf", "-W", "-l", "", NULL};
+
+	(void)argc;
+	command[3] = argv[1];
+	if (execve("/usr/bin/readelf", command, env) == -1)
+	{
+		perror("execv");
+		return (EXIT_FAILURE);
+	}
+	return (EXIT_SUCCESS);
+}
