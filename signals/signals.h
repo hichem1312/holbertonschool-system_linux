c8b57be6b48661c4ec050b392a62b06b1438bc1b
hichem <hichem@hichem.com>
2023-06-14 22:01:28 +0100

    header file

--- signals/signals.h
+++ signals/signals.h
@@ -1,6 +1,11 @@
 #ifndef SIGNALS_H
 #define SIGNALS_H
 #include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+#include <string.h>
 #include "signal.h"
 int handle_signal(void);
 void (*current_handler_signal(void))(int);
