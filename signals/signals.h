31cc61718ebea1c7680706c5fb836eb975e0564b
hichem <hichem@hichem.com>
2023-06-15 21:55:22 +0100

    header file

--- signals/signals.h
+++ signals/signals.h
@@ -12,5 +12,4 @@ void (*current_handler_signal(void))(int);
 int handle_sigaction(void);
 void (*current_handler_sigaction(void))(int);
 int trace_signal_sender(void);
-int main(int argc, char *argv[]);
 #endif
