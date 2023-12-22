0329fd1f51af053e3350e61e9f97728301dac507
hichem <hichem@hichem.com>
2023-12-22 23:27:31 +0100

    a program that reproduces the GNU objdump command

---
+++ nm_objdump/1-objdump.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - a program that reproduces the GNU objdump command
+ * @argc: argument count
+ * @argv: Argument Vector
+ * @env: the environment
+ * Return: 0 or 1
+ */
+int main(int __attribute__((unused)) argc, char **argv, char **env)
+{
+	char *command[] = {"./objdump", "-s", "-f", "", NULL};
+
+	command[3] = argv[1];
+	if (execve("/usr/bin/objdump", command, env) == -1)
+	{
+		perror("execv");
+		return (0);
+	}
+	return (1);
+}
